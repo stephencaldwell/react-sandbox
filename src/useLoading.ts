cd488fef93b054a134e67b5fbb84f1bd0ae4405a
Stephen Caldwell <stephen@savemysales.co>
2019-12-03 12:07:06 -0500

    Adds console logs

--- src/useLoading.ts
+++ src/useLoading.ts
@@ -5,11 +5,13 @@ export function useLoading<T extends any[], TResult>(
 ): [boolean, (...args: T) => Promise<TResult>] {
   const [isLoading, setIsLoading] = useState(false);
   const wrappedFunction = async (...args: T) => {
+    console.log("setting isLoading to true (no setTimeout)");
     setIsLoading(false);
     try {
       const response = await f(...args);
       return response;
     } finally {
+      console.log("setting isLoading to false (no setTimeout)");
       setIsLoading(false);
     }
   };
@@ -23,12 +25,16 @@ export function useLoadingTimeout<T extends any[], TResult>(
   const [isLoading, setIsLoading] = useState(false);
   const wrappedFunction = (...args: T) => {
     return new Promise<TResult>((resolve, reject) => {
+      console.log("setting isLoading to true (setTimeout)");
       setIsLoading(true);
       setTimeout(() =>
         f(...args)
           .then(resolve)
           .catch(reject)
-          .finally(() => setIsLoading(false))
+          .finally(() => {
+            console.log("setting isLoading to false (setTimeout)");
+            setIsLoading(false);
+          })
       );
     });
   };
